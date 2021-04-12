4ad5c29091a6572396ad1538c781e5e88791eb83
datadavev <605409+datadavev@users.noreply.github.com>
2021-04-11 22:05:46 -0400

    Added status stats to ui

--- web/src/js/load_data.js
+++ web/src/js/load_data.js
@@ -2,8 +2,23 @@
 export let NAANS = [];
 const NAANS_SOURCE = "https://raw.githubusercontent.com/datadavev/naanj/main/data/naanj.json";
 
+const stat_messages = {
+    '-1': "TLS error without validation",
+    '0': "Client was unable to establish a connection",
+    '200': "OK",
+    '400': "Bad Request",
+    '401': "Unauthorized",
+    '403': "Forbidden",
+    '404': "Not Found",
+    '406': "Not Acceptable",
+    '500': "Internal Server Error",
+    '502': "Bad Gateway",
+    '503': "Service Unavailable",
+}
+
 function processNaanList(data) {
-    let meta = data.erc
+    let meta = data.erc;
+    let stats = {};
     meta.when = moment(meta.when).format("YYYY-MM-DD");
     let error_count = 0
     for (let naa of data.naa) {
@@ -21,16 +36,24 @@ function processNaanList(data) {
         if (entry.status !== 200) {
             error_count += 1;
         }
+        stats[entry.status] = (stats[entry.status] || 0) + 1;
         NAANS.push(entry)
+    }
+    let astats = [];
+    for (let s in stats) {
+        astats.push( [s, stats[s], (stat_messages[s] || "")]);
     }
     meta.num_errors = error_count;
     meta.naanj_source = NAANS_SOURCE;
     let event = new CustomEvent("naans-load", {
         detail: {
-            meta: meta
+            meta: meta,
+            stats: stats,
+            astats: astats
         }
     });
     window.dispatchEvent(event);
+    window.stats = stats;
     console.log(NAANS.length)
     return NAANS;
 }
@@ -40,3 +63,4 @@ export function loadNaans() {
         .then(response => response.json())
         .then(data => processNaanList(data))
 }
+
